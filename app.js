ad9e8359341e24b9b7131394484d15faefa27bd7
Jarod Means <jsmeans2@gmail.com>
2016-08-05 15:42:52 -0500

    small fix

--- app.js
+++ app.js
@@ -12,15 +12,3 @@ http.createServer(function(request, response){
 
 console.log('Server running at localhost:8080');
 
-/**
-* When the JSON body is fully recieved the 
-* the "end" event is triggered and the full body
-* is given to the handler or callback
-**/
-//cityProfile.on("end", console.dir);
-
-/**
-* If a parsing, network or HTTP error occurs an
-* error object is passed in to the handler or callback
-**/
-//clear
